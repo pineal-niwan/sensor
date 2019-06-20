923b89ac6ef22513d3ca2b815fa3be9e7f308536
zhangkun <zhangkun@zeroenergy.cc>
2019-06-21 00:03:30 +0800

    in progress

--- db/transaction.go
+++ db/transaction.go
@@ -1,21 +1,52 @@
 package db
 
 import (
+	"github.com/go-errors/errors"
 	"github.com/jinzhu/gorm"
 	"github.com/pineal-niwan/sensor/logger"
 )
 
+//处理db的函数
 type HandleDbFunc func(*gorm.DB) error
 
-func ExecuteTransaction(db *gorm.DB, handler HandleDbFunc, anchor string, logger logger.ILogger) error {
+//封装的事务,没有save point相关,任何一个步骤出错就回滚整个事务
+func ExecuteTransaction(db *gorm.DB, anchor string, logger logger.ILogger, handlerList ...HandleDbFunc) (err error) {
 	txn := db.Begin()
-	err := txn.Error
+	err = txn.Error
+
 	if err != nil {
 		logger.Errorf(`%s: 开始事务失败: %+v`, anchor, err)
 		return err
 	}
+
 	defer func() {
-		err = recover()
+		r := recover()
+		if r != nil {
+			rErr := errors.Wrap(r, 3)
+			logger.Errorf(`%s: panic: %+v`, anchor, r)
+			logger.Errorf(`%s: panic stack:%+v`, anchor, string(rErr.Stack()))
+			err = rErr
+		}
+
+		if err != nil {
+			logger.Errorf(`%s: 出错,事务需要回滚:%+v`, anchor, err)
+			txn.Rollback()
+		} else {
+			err = txn.Commit().Error
+			if err != nil {
+				logger.Errorf(`%s: 提交事务出错:%+v`, anchor, err)
+			} else {
+				logger.Debugf(`%s: 提交事务成功`, anchor)
+			}
+		}
 	}()
 
+	for _, handler := range handlerList {
+		err = handler(txn)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
 }
