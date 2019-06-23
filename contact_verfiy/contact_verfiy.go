e261ac1812562b9994bfdc81f918b9546a9b6432
zhangkun <zhangkun@zeroenergy.cc>
2019-06-23 20:02:56 +0800

    add phone/email verify interface

---
+++ contact_verfiy/contact_verfiy.go
@@ -0,0 +1,11 @@
+package contact_verfiy
+
+// 验证电话号码
+func VerifyMobilePhoneContact(phoneNum string) bool {
+	return true
+}
+
+// 验证电子邮箱
+func VerifyEmailContact(email string) bool{
+	return true
+}
